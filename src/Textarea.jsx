bd666cbfbff75cea9855aae60ac7e6848f584202
voja <voja@gmail.com>
2024-05-24 11:50:56 +0200

    04 Controlled textarea and basic validation

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 function Textarea() {
+    const [text, setText] = useState("");
+    const [showWarning, setShowWarning] = useState(false);
+
+    function handleChange(e) {
+        // console.log(e.target.value);
+
+        let newText = e.target.value;
+        if (newText.match(/<script>/i)) {
+            setShowWarning(true);
+            newText = newText.replace(/<script>/i, "");
+        }
+
+        setText(newText);
+    }
+
     return (
         <div className="textarea">
             <textarea
+                value={text}
                 className="textarea"
                 placeholder="Enter your text here"
                 spellCheck="false"
+                onChange={handleChange}
             ></textarea>
+
+            {showWarning && <Warning />}
         </div>
     );
 }
---
+++ src/Warning.jsx
@@ -0,0 +1,5 @@
+function Warning() {
+    return <p className="warning">Do not use script tags!</p>;
+}
+
+export default Warning;
