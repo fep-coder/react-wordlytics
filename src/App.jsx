cb4270c0306bb8a34be5fbd08a6de1d0e3675a41
voja <voja@gmail.com>
2024-05-24 11:19:15 +0200

    02 App jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 function App() {
-    return <div>App</div>;
+    return (
+        <>
+            <header>
+                <div className="bg"></div>
+                <h1>WORDLYTICS</h1>
+            </header>
+            <main className="container">
+                <div className="textarea">
+                    <textarea
+                        className="textarea"
+                        placeholder="Enter your text here"
+                        spellCheck="false"
+                    ></textarea>
+                </div>
+
+                <section className="stats">
+                    <section className="stat">
+                        <span className="stat-number">0</span>
+                        <h4>Words</h4>
+                    </section>
+                    <section className="stat">
+                        <span className="stat-number">0</span>
+                        <h4>Characters</h4>
+                    </section>
+                    <section className="stat">
+                        <span className="stat-number">2200</span>
+                        <h4>Instagram / Tiktok</h4>
+                    </section>
+                    <section className="stat">
+                        <span className="stat-number">500</span>
+                        <h4>Pinterest</h4>
+                    </section>
+                </section>
+            </main>
+        </>
+    );
 }
 
 export default App;
