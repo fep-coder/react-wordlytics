d0518eed3ecb5aab02697fe0bb548416a41d98d3
voja <voja@gmail.com>
2024-05-24 12:02:20 +0200

    06 Other stats

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ import { useState } from "react";
 
 function App() {
     const [text, setText] = useState("");
+
     const numberOfCharacters = text.length;
+    const numberOfWords = text
+        .trim()
+        .split(/\s+/)
+        .filter((x) => x != "").length;
+    const instagramCharactersLeft = 2200 - numberOfCharacters;
+    const pinterestCharactersLeft = 500 - numberOfCharacters;
 
     return (
         <>
@@ -14,9 +21,12 @@ function App() {
                 <Textarea text={text} setText={setText} />
                 <section className="stats">
                     <Stat text="Characters" number={numberOfCharacters} />
-                    <Stat text="Words" number="0" />
-                    <Stat text="Instagrams / Tiktok" number="2200" />
-                    <Stat text="Pinterest" number="500" />
+                    <Stat text="Words" number={numberOfWords} />
+                    <Stat
+                        text="Instagram / Tiktok"
+                        number={instagramCharactersLeft}
+                    />
+                    <Stat text="Pinterest" number={pinterestCharactersLeft} />
                 </section>
             </main>
         </>
